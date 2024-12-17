39301d0ade0e815f27179ebcb9a1b24a9d660c83
NishanthVaidya <nishanthvaidya1998@gmail.com>
2024-12-17 13:43:04 -0500

    Main Overflow Issue Resolved, added Overflow visible in global css

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,7 +29,7 @@ const Hero = () => {
        *  0.2 to 0.03
        */}
       <div
-        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+        className="h-screen w-full dark:bg-black-100 overflow-visible bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
       >
         {/* Radial gradient for the container to give a faded look */}
@@ -60,7 +60,7 @@ const Hero = () => {
             Technology isn’t about complexity—it’s about making life simpler and smarter.
           </p>
 
-          <a href="#about">
+          <a href="#about" className="relative-container">
             <MagicButton
               title="Show my work"
               icon={<FaLocationArrow />}
